fce207acb0939aa03ff1b585b6d7d8c915219888
kobewi <kobewi4e@gmail.com>
2022-09-03 14:32:06 +0200

    Upgrade to Godot 4.0

--- addons/SliderLabel/SliderLabel.gd
+++ addons/SliderLabel/SliderLabel.gd
@@ -1,43 +1,47 @@
-tool
+@tool
 extends Label
 
 const SLIDER_WARNING = "SliderLabel needs to be a child of a Slider control (HSlider or VSlider)."
-const SLIDER_WARNING2 = "custom_slider_path needs to point to a Slider control (HSlider or VSlider)."
+const SLIDER_WARNING2 = "custom_slider_path needs to point to a valid Slider control (HSlider or VSlider)."
 
 enum VisibilityRule {ON_CLICK, ON_HOVER, ON_FOCUS, ALWAYS}
 enum Placement {TOP_RIGHT, BOTTOM_LEFT}
 
-export(VisibilityRule) var visibility_rule: int = VisibilityRule.ON_HOVER
-export(Placement) var placement: int = Placement.TOP_RIGHT
-export var separation := 4
-export var custom_format := ""
-export var custom_slider_path: NodePath setget set_custom_path
+@export var visibility_rule: VisibilityRule = VisibilityRule.ON_HOVER
+@export var placement: Placement = Placement.TOP_RIGHT
+@export var separation := 4
+@export var custom_format := ""
+@export_node_path(Slider) var custom_slider_path = NodePath():
+	set(path):
+		custom_slider_path = path
+		update_configuration_warnings()
 
 var slider: Slider
 var vertical: bool
 
 func _enter_tree() -> void:
-	if not has_meta("_edit_initialized_"):
-		set_meta("_edit_initialized_", true)
-		align = Label.ALIGN_CENTER
-		valign = Label.VALIGN_CENTER
+	if not has_meta(&"_edit_initialized_"):
+		set_meta(&"_edit_initialized_", true)
+		horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+		vertical_alignment = VERTICAL_ALIGNMENT_CENTER
 		size_flags_horizontal = SIZE_SHRINK_CENTER
 		text = "100"
 
 func _ready() -> void:
-	if Engine.editor_hint:
+	if Engine.is_editor_hint():
 		return
 	
 	if custom_slider_path.is_empty():
 		slider = get_parent() as Slider
+		assert(slider != null, SLIDER_WARNING)
 	else:
 		slider = get_node(custom_slider_path) as Slider
-	assert(slider != null, SLIDER_WARNING)
+		assert(slider != null, SLIDER_WARNING2)
 	
 	if slider is VSlider:
 		vertical = true
 	
-	slider.connect("value_changed", self, "update_with_discard")
+	slider.value_changed.connect(update_with_discard)
 	
 	if visibility_rule == VisibilityRule.ALWAYS:
 		show()
@@ -46,13 +50,13 @@ func _ready() -> void:
 		
 		match visibility_rule:
 			VisibilityRule.ON_CLICK:
-				slider.connect("gui_input", self, "_on_slider_gui_input")
+				slider.gui_input.connect(_on_slider_gui_input)
 			VisibilityRule.ON_HOVER:
-				slider.connect("mouse_entered", self, "_on_slider_hover_focus", [true])
-				slider.connect("mouse_exited", self, "_on_slider_hover_focus", [false])
+				slider.mouse_entered.connect(_on_slider_hover_focus.bind(true))
+				slider.mouse_exited.connect(_on_slider_hover_focus.bind(false))
 			VisibilityRule.ON_FOCUS:
-				slider.connect("focus_entered", self, "_on_slider_hover_focus", [true])
-				slider.connect("focus_exited", self, "_on_slider_hover_focus", [false])
+				slider.focus_entered.connect(_on_slider_hover_focus.bind(true))
+				slider.focus_exited.connect(_on_slider_hover_focus.bind(false))
 	
 	update_label()
 
@@ -67,7 +71,7 @@ func _on_slider_hover_focus(hover: bool):
 
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_PARENTED:
-		update_configuration_warning()
+		update_configuration_warnings()
 
 func update_with_discard(discard):
 	update_label()
@@ -76,38 +80,37 @@ func update_label():
 	if not is_visible_in_tree():
 		return
 	
-	if custom_format.empty():
+	if custom_format.is_empty():
 		text = str(slider.value)
 	else:
 		text = custom_format % slider.value
 	
 	hide()
 	show()
-	rect_size = Vector2()
+	size = Vector2()
 	
-	var grabber_size := slider.get_icon("Grabber").get_size()
+	var grabber_size := slider.get_theme_icon(&"Grabber").get_size()
 	if vertical:
-		rect_position.y = (1.0 - slider.ratio) * (slider.rect_size.y - grabber_size.y) + grabber_size.y * 0.5 - rect_size.y * 0.5
+		position.y = (1.0 - slider.ratio) * (slider.size.y - grabber_size.y) + grabber_size.y * 0.5 - size.y * 0.5
 		if placement == Placement.TOP_RIGHT:
-			rect_position.x = slider.rect_size.x + separation
+			position.x = slider.size.x + separation
 		else:
-			rect_position.x = -rect_size.x - separation
+			position.x = -size.x - separation
 	else:
-		rect_position.x = slider.ratio * (slider.rect_size.x - grabber_size.x) + grabber_size.x * 0.5 - rect_size.x * 0.5
+		position.x = slider.ratio * (slider.size.x - grabber_size.x) + grabber_size.x * 0.5 - size.x * 0.5
 		if placement == Placement.TOP_RIGHT:
-			rect_position.y = -rect_size.y - separation
+			position.y = -size.y - separation
 		else:
-			rect_position.y = slider.rect_size.y + separation
+			position.y = slider.size.y + separation
 
-func _get_configuration_warning() -> String:
+func _get_configuration_warnings() -> PackedStringArray:
+	var ret: PackedStringArray
+	
 	if custom_slider_path.is_empty():
 		if not get_parent() is Slider:
-			return SLIDER_WARNING
+			ret.append(SLIDER_WARNING)
 	else:
 		if not get_node(custom_slider_path) is Slider:
-			return SLIDER_WARNING2
-	return ""
-
-func set_custom_path(path: NodePath):
-	custom_slider_path = path
-	update_configuration_warning()
+			ret.append(SLIDER_WARNING2)
+	
+	return ret
--- addons/SliderLabel/plugin.gd
+++ addons/SliderLabel/plugin.gd
@@ -1,8 +1,8 @@
-tool
+@tool
 extends EditorPlugin
 
 func _enter_tree() -> void:
 	add_custom_type("SliderLabel", "Label", preload("res://addons/SliderLabel/SliderLabel.gd"), preload("res://addons/SliderLabel/SliderLabel.svg"))
 
 func _exit_tree() -> void:
-	pass
+	remove_custom_type("SliderLabel")
